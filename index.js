3ad3f2b315ada16748ecb219481f879eb9f40752
johnsonfash <fashanutosin7@gmail.com>
2024-05-26 05:03:23 +0100

    fixd subdomain issue

--- index.js
+++ index.js
@@ -8,23 +8,25 @@ const PORT = process.env.PORT || 3001;
 const static = express.static(path.join(__dirname, 'build'));
 
 app.use('/', (req, res, next) => {
-  const hostname = req.hostname;
-  const [subdomain, ..._] = hostname.split('.');
-  if (subdomain == 'error') {
-    res.sendFile(path.join(__dirname, 'error.html'));
-  } else {
-    static(req, res, next);
-  }
+  // const hostname = req.hostname;
+  // const [subdomain, ..._] = hostname.split('.');
+  // if (subdomain == 'error') {
+  //   res.sendFile(path.join(__dirname, 'error.html'));
+  // } else {
+  //   static(req, res, next);
+  // }
+  res.sendFile(path.join(__dirname, 'error.html'));
 });
 
 app.use('*', function (_, res) {
-  const hostname = req.hostname;
-  const [subdomain, ..._] = hostname.split('.');
-  if (subdomain == 'error') {
-    res.sendFile(path.join(__dirname, 'error.html'));
-  } else {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  }
+  // const hostname = req.hostname;
+  // const [subdomain, ..._] = hostname.split('.');
+  // if (subdomain == 'error') {
+  //   res.sendFile(path.join(__dirname, 'error.html'));
+  // } else {
+  //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // }
+  res.sendFile(path.join(__dirname, 'error.html'));
 });
 
 
