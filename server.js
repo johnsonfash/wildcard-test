2cf50de76a08394176f495471eb6a182fc0eab72
johnsonfash <fashanutosin7@gmail.com>
2024-05-26 06:22:16 +0100

    #6

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -14,6 +15,20 @@ app.use('/', (req, res, next) => {
   //   res.sendFile(path.join(__dirname, 'error.html'));
   // } else {
   // }
+  fs.readdir(__dirname, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    const folders = files.filter((file) => {
+      const filePath = path.join(directoryPath, file);
+      const stats = fs.statSync(filePath);
+      return stats.isDirectory();
+    });
+
+    console.log('Folders in the directory:', JSON.stringify(folders));
+  });
   static(req, res, next);
   // res.sendFile(path.join(__dirname, 'error.html'));
 });
@@ -25,6 +40,20 @@ app.use('*', function (_, res) {
   //   res.sendFile(path.join(__dirname, 'error.html'));
   // } else {
   // }
+  fs.readdir(__dirname, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    const folders = files.filter((file) => {
+      const filePath = path.join(directoryPath, file);
+      const stats = fs.statSync(filePath);
+      return stats.isDirectory();
+    });
+
+    console.log('Folders in the directory:', JSON.stringify(folders));
+  });
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
